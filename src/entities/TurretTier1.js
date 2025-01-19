58db15d57efd5334c79a62f427bb9d606d3cebdf
Nicolas Vaillancourt <nvaillan@ucsc.edu>
2025-01-19 08:41:49 -0800

    slight refactor of turrettier1 and 2

--- src/entities/TurretTier1.js
+++ src/entities/TurretTier1.js
@@ -1,18 +1,19 @@
 class TurretTier1 extends Turret {
-    constructor(scene, x, y) {
-        let params = {
-            name: "turrettier1",
-            targetRadius: 6,
-            logisticRadius: 10,
-            scale: 1,
-            fireRate: 1,
-            muzzleVel: 10,
-            shootOffset: [new Phaser.Math.Vector2(0, -0.5)],
-            projectileType: BulletTier1,
-            maxHealth: 10,
-            healthRegenRate: 0.01,
-            base_dps: 1
-        }
-        super(scene, x, y, params)
+    static params = {
+        name: "turrettier1",
+        targetRadius: 6,
+        logisticRadius: 10,
+        scale: 1,
+        fireRate: 1,
+        muzzleVel: 10,
+        shootOffset: [new Phaser.Math.Vector2(0, -0.5)],
+        projectileType: BulletTier1,
+        maxHealth: 10,
+        healthRegenRate: 0.01,
+        base_dps: 1
+    }
+
+    constructor(scene, x, y, params) {
+        super(scene, x, y, {...TurretTier1.params, ...params})
     }
 }
--- src/entities/TurretTier2.js
+++ src/entities/TurretTier2.js
@@ -1,18 +1,19 @@
 class TurretTier2 extends Turret {
-    constructor(scene, x, y) {
-        let params = {
-            name: "turrettier2",
-            targetRadius: 10,
-            logisticRadius: 15,
-            scale: 1.5,
-            fireRate: 2,
-            muzzleVel: 15,
-            shootOffset: [new Phaser.Math.Vector2(0.1, -0.5), new Phaser.Math.Vector2(-0.1, -0.5)],
-            projectileType: BulletTier2,
-            maxHealth: 40,
-            healthRegenRate: 0.01,
-            base_dps: 4
-        }
-        super(scene, x, y, params)
+    static params = {
+        name: "turrettier2",
+        targetRadius: 10,
+        logisticRadius: 15,
+        scale: 1.5,
+        fireRate: 2,
+        muzzleVel: 15,
+        shootOffset: [new Phaser.Math.Vector2(0.1, -0.5), new Phaser.Math.Vector2(-0.1, -0.5)],
+        projectileType: BulletTier2,
+        maxHealth: 40,
+        healthRegenRate: 0.01,
+        base_dps: 4
+    }
+
+    constructor(scene, x, y, params) {
+        super(scene, x, y, {...TurretTier2.params, ...params})
     }
 }
